6da11253c8b084a47e3bf4f3ce3fa65bd3221990
Luiz Carlos Rezende da Silva <214377491+luizcrezende@users.noreply.github.com>
2026-05-24 18:47:50 -0300

    Add Cloudflare Pages ASK widget middleware

---
+++ functions/_middleware.js
@@ -0,0 +1,18 @@
+const ASK_WIDGET_SNIPPET = '<script src="/mell-atendimento-widget.js" defer></script>';
+
+export async function onRequest(context) {
+  const response = await context.next();
+  const contentType = response.headers.get("content-type") || "";
+
+  if (!contentType.includes("text/html")) {
+    return response;
+  }
+
+  return new HTMLRewriter()
+    .on("body", {
+      element(element) {
+        element.append(ASK_WIDGET_SNIPPET, { html: true });
+      },
+    })
+    .transform(response);
+}
